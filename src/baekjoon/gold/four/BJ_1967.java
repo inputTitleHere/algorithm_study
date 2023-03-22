37e5531eb4f14a39a8fa2fd470d8d1e041c7435e
백승윤 <seanbryan97@naver.com>
2023-03-22 14:14:24 +0900

    new algo for 1967

--- src/baekjoon/gold/four/BJ_1967.java
+++ src/baekjoon/gold/four/BJ_1967.java
@@ -11,17 +11,19 @@ public class BJ_1967 {
 	
 	static List<Integer> leaf;
 	static boolean[] visited;
-	static List<ArrayList<int[]>> tree;
+//	static List<ArrayList<int[]>> tree;
+	static ArrayList<int[]>[] tree;
 	static int maxLen=0;
+	static int tmp=0;
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer stk = null;
 		int n = Integer.parseInt(br.readLine());
 		
-		tree = new ArrayList<>(); // [to,dist]
+		tree = new ArrayList[n+1]; // [to,dist]
 		for(int i=0;i<=n;i++) {
-			tree.add(new ArrayList<>());
+			tree[i]=new ArrayList<int[]>();
 		}
 		
 		leaf = new ArrayList<>();
@@ -32,30 +34,31 @@ public class BJ_1967 {
 			int to = Integer.parseInt(stk.nextToken());
 			int dist = Integer.parseInt(stk.nextToken());
 			
-			tree.get(from).add(new int[] {to,dist});
-			tree.get(to).add(new int[] {from,dist});
+			tree[from].add(new int[] {to,dist});
+			tree[to].add(new int[] {from,dist});
 			
 		}
 		visited = new boolean[n+1];
+		// find farthest leaf from root
+		visited[1]=true;
+		dfs(1,0);
 		
-		// 리프노드 추출
-		for(int i=0;i<n;i++) {
-			if(tree.get(i).size()==1) {
-				// leaf노드이면
-				visited[i]=true;
-				dfs(i,0);
-			}
-		}
+		visited[1]=false;
+		visited[tmp]=true;
+		dfs(tmp,0);
+		
 		
 		System.out.println(maxLen);
 	}
 	static void dfs(int currNode, int len) {
 		
-		List<int[]> near = tree.get(currNode);
+		List<int[]> near = tree[currNode];
 		if(near.size()==1) { // 끝 노드임
-			maxLen=Math.max(maxLen, len);
+			if(maxLen<len) {
+				tmp=currNode;
+				maxLen=Math.max(maxLen, len);
+			}
 		}
-		
 		for(int[] i : near) {
 			if(!visited[i[0]]) {
 				visited[i[0]]=true;
