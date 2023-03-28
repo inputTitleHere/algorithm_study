6e2c86693d1556b9e3ebd4054a59723b94087673
inputTitleHere <seanbryan97@naver.com>
2023-03-28 20:27:51 +0900

    BJ_2579

--- src/baekjoon/silver/three/BJ_2579.java
+++ src/baekjoon/silver/three/BJ_2579.java
@@ -3,6 +3,7 @@ package baekjoon.silver.three;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class BJ_2579 {
 	
@@ -12,29 +13,16 @@ public class BJ_2579 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		stair = new int[300];
+		stair = new int[301];
 		dp=new int[306];
-		for(int i=0;i<n;i++) {
+		for(int i=1;i<=n;i++) {
 			stair[i]=Integer.parseInt(br.readLine());
 		}
-		if(n==2) {
-			System.out.println(stair[0]+stair[1]);
-			return;
-		}else if (n==1) {
-			System.out.println(stair[0]);
-			return;
-		}
-		for(int i=0;i<n-2;i++) {
-			dp[n+]
-		}
-		
-		
+		dp[1]=stair[1];
+		dp[2]=stair[2]+dp[1];
+		for(int i=3;i<=n;i++) {
+			dp[i]=Math.max(dp[i-3]+stair[i-1]+stair[i], dp[i-2]+stair[i]);
+		}	
+		System.out.println(dp[n]);
 	}
-	
-	static int max(int... items) {
-		
-		
-		return 0;
-	}
-	
 }
