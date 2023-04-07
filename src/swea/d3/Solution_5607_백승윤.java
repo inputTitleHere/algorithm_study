8fa26250348283c8ec7efc9d81710d53ab040bdd
inputTitleHere <seanbryan97@naver.com>
2023-04-07 09:44:10 +0900

    2558 bj solved

--- src/swea/d3/Solution_5607_백승윤.java
+++ src/swea/d3/Solution_5607_백승윤.java
@@ -4,42 +4,55 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Solution_5607_백승윤 {
-	static long[] helpme;
-	static int ptr=1;
+	static long[] fact;
+	static int ptr = 1;
 	static final int MAGIC = 1234567891;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		helpme = new long[1000001];
-		helpme[0] = 1;
+		fact = new long[1000001];
+		fact[0] = 1;
 		int tc = Integer.parseInt(br.readLine());
-		int rt = tc;
-		while (tc-- > 0) {
-			sb.append("#").append(rt - tc).append(" ");
+		for (int t = 1; t <= tc; t++) {
+
+			sb.append("#").append(t).append(" ");
 			StringTokenizer stk = new StringTokenizer(br.readLine());
-			
+
 			int n = Integer.parseInt(stk.nextToken());
 			int r = Integer.parseInt(stk.nextToken());
+
+			long res = ncr(n,r);
 			
-			// n! / r!*(n-r)!
-			long res = (factorial(n)/(factorial(r)*factorial(n-r)))%MAGIC;
+
 			sb.append(res).append("\n");
 		}
 		System.out.println(sb.toString());
 	}
 
-	static long factorial(int n) {
-		
-		if(n>ptr) {
-			while(n>=ptr) {
-				helpme[ptr]=(helpme[ptr-1]*ptr)%MAGIC;
-				ptr++;
-			}
+	static long ncr(int n, int r) {
+		if (r == 0) {
+			return 1L;
 		}
-		System.out.println("run for "+n);
-		System.out.println(helpme[n]);
-		return helpme[n];
+		for (int i = ptr; i <= n; i++, ptr++) {
+			fact[i] = fact[i - 1] * i % MAGIC;
+		}
+
+		return (fact[n] * power(fact[r], MAGIC - 2) % MAGIC * power(fact[n - r], MAGIC - 2) % MAGIC) % MAGIC;
+	}
+
+	static long power(long x, long y) {
+		long res = 1L;
+
+		x = x % MAGIC;
+		while (y > 0) {
+			if (y % 2 == 1) {
+				res = (res * x) % MAGIC;
+			}
+			y >>= 1;
+			x = (x * x) % MAGIC;
+		}
+		return res;
 	}
 
 }
