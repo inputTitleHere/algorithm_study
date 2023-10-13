6ef75d394e72e07a95b392bad057fd1c6efbc2ad
inputTitleHere <seanbryan97@naver.com>
2023-10-13 17:54:52 +0900

    swea 2112 solving

--- src/codetree/trainingfield/samsung1/Solution.java
+++ src/codetree/trainingfield/samsung1/Solution.java
@@ -52,55 +52,68 @@ public class Solution {
 
     // board[버전][행][열]
     static void dfs(int step, int[][][] board, int mode, int score) {
+//        System.out.println("s >> " + step + " mode >> " + mode);
         if (step == n) {
+//            System.out.println("ITS OVER ANAKIN! I HAVE THE HIGH GROUND!!");
+//            printBoard(board[mode]);
             point = Math.max(point, score);
+//            System.out.println("SCORE = " + score);
             return;
         }
         int k = order[step][0];
         int c = order[step][1];
 //        System.out.println("k = " + k + "c = " + c);
-        if (c == 0) {
+        if (c == 0) { // 무작위 배치인 경우
             for (int i = 0; i < 4; i++) {
                 int[][] newBoard = snapshot(board[mode]);
 //                System.out.println("k = " + k + "c(i) = " + i);
-                int localScore = score;
-                board[mode] = newBoard;
-                placeBlock(k, i, board[mode]);
-//                System.out.println("_+_+_+_+_+_+_+");
+//                System.out.println("BEFORE+++++++++++++");
 //                printBoard(board[mode]);
+                int localScore = score;
+                placeBlock(k, i, board[mode]);
                 localScore += checkRow(board[mode]);
                 gravitate(board[mode]);
                 moveBlock(board, mode);
                 mode = (mode + 1) % 2;
                 gravitate(board[mode]);
                 localScore += checkRow(board[mode]);
+                gravitate(board[mode]);
+//                System.out.println("AFTER+++++++++++++");
+//                printBoard(board[mode]);
                 dfs(step + 1, board, mode, localScore);
+                // remove last placed block
+                board[mode] = newBoard;
+
+
             }
-        } else {
+        } else { // 지정배치인경우
             int localScore = score;
             placeBlock(k, c - 1, board[mode]);
 //            System.out.println("=========================================");
 //            printBoard(board[mode]);
 
-//            System.out.println("+++CHECK ROW");
             localScore += checkRow(board[mode]);
+//            System.out.println("+++CHECK ROW");
 //            printBoard(board[mode]);
 
-//            System.out.println("+++ GRAVITATE");
             gravitate(board[mode]);
+//            System.out.println("+++ GRAVITATE");
 //            printBoard(board[mode]);
 
-//            System.out.println("+++ MOVEEEEE");
             moveBlock(board, mode);
             mode = (mode + 1) % 2;
+//            System.out.println("+++ MOVEEEEE");
 //            printBoard(board[mode]);
 
-//            System.out.println("+++ GRAVITATE 2");
             gravitate(board[mode]);
+//            System.out.println("+++ GRAVITATE 2");
 //            printBoard(board[mode]);
 
 //            System.out.println("+++ CHECK ROW 2");
             localScore += checkRow(board[mode]);
+
+            gravitate(board[mode]);
+
             dfs(step + 1, board, mode, localScore);
         }
     }
@@ -154,21 +167,19 @@ public class Solution {
         }
     }
 
+    // 규칙에 따라 블럭 옮기기
     static void moveBlock(int[][][] board, int mode) {
         int[][] ori = board[mode];
-        int[][] cpy = board[(mode + 1) % 2];
+        int[][] cpy = new int[board[mode].length][board[mode][0].length];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < 4; j++) {
                 int no = ori[i][j];
                 if (no != 0) {
-//                    System.out.println("HEEEEEEEEEEEEEEEEELP no = "+no);
                     for (int k : moveReference[no]) {
                         int[] move = direction[k - 1];
-//                        System.out.print(Arrays.toString(move));
                         int ni = i + move[0];
                         int nj = j + move[1];
-                        if (ni > -1 && ni < 4 && nj > -1 && nj < 4) {
-//                            System.out.println("\nmovable = " + k + " of => " + Arrays.toString(move));
+                        if (ni > -1 && ni < n && nj > -1 && nj < 4) {
                             if (cpy[ni][nj] != 0) {
                                 cpy[ni][nj] = Math.min(cpy[ni][nj], ori[i][j]);
                             } else {
@@ -180,6 +191,7 @@ public class Solution {
                 }
             }
         }
+        board[(mode + 1) % 2] = cpy;
         board[mode] = new int[n + 1][4];
     }
 }
